4d92649e957a2859819102f576985d345f2687de
kgb <kgb@Kesters-iMac.fios-router.home>
2021-10-02 23:21:32 -0400

    Changed About page

--- pages/about.js
+++ pages/about.js
@@ -10,17 +10,18 @@ const About = () => {
 				backgroundHeight="30vh"
 			/>
       <div className="container">
-        <h2>About</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in cul</p>
-
-        <p>pa qui officia deserunt mollit anim id est laborum.</p>
-
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+       <h2>The WHW of ISY</h2> 
+        <p>Why:</p>
+        <p>’Social Health level’ of society is a measurement indicator for  (individual health) how well a person’s ability to live a thriving life is and (community health) how well a society can provide for a persons ability to live a thriving life.This idea of Social Health is the ethos of ISY.</p>
+        <p>I feel as though the enjoyment of social healthful activities should not be limited, influenced or directed by persons practicing sociopathic racist, sexist, xenophobic, etc. behaviors, attitudes or perspectives. Sociopathic is defined as actions, behaviors taken by a person to their satisfaction that threatens, challenges or impedes, reduces someones social health level. Although sociopathic influence persists, we can neuter the impact of that sociopathy by using tools to strengthen our ability to participate in and live thriving lives. Participation in Social healthful activities is one way to blunt that impact as SHA are essential for personal, mental, emotional, physical and community health and ISY will be the conduit strengthening social health for POC</p>
+        <p>How:</p>
+        <p>OC OC can unify their voices and concerns through a medium, in this case, ISY Network. Advocates can also hear these voices.
+        Advocates (groups, individuals) can then go to Businesses and Governments to show that POC have unified their voices for a certain issue.Businesses and Government can then knowledgeably make sure that POC voices have been heard through actions that satisfy the needs of POC
+        POC solidify and validate their voice as a powerful tool to strengthen their thriving lives and social health.</p>
+        <p>What: </p>
+        <p>ISY is organization that aims to help POC find support , guidance and resources for enjoying activities like cycling , hiking, camping, sewing,  gaming, birding, etc. Make sure POC get the benefits of social healthful activities.</p>
 
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
-        </p>
       </div>
     </>
   )
